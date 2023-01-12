1b32277fd4748cc30cc6793be338e96d55b12b2b
colin_greens <colin.j.benson@gmail.com>
2023-01-11 22:34:48 -0800

    added input validation for new place

--- udemy-react-app/src/places/pages/NewPlace.jsx
+++ udemy-react-app/src/places/pages/NewPlace.jsx
@@ -1,3 +1,18 @@
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewPlace.css";
+
 export default function NewPlace() {
-  return <h2>New Place Works!</h2>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        onChange={""}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
 }
